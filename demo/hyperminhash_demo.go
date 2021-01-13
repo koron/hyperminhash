79b138ba3d0816e677f53f08504a474017f368ce
MURAOKA Taro <koron.kaoriya@gmail.com>
2021-01-13 13:58:26 +0900

    compare with axiomhq/hyperloglog too

--- demo/hyperminhash_demo.go
+++ demo/hyperminhash_demo.go
@@ -5,8 +5,9 @@ import (
 	"strconv"
 	"time"
 
-	pcgr "github.com/dgryski/go-pcgr"
+	"github.com/axiomhq/hyperloglog"
 	"github.com/axiomhq/hyperminhash"
+	pcgr "github.com/dgryski/go-pcgr"
 )
 
 func estimateError(got, exp uint64) float64 {
@@ -25,8 +26,8 @@ func main() {
 	for k := int64(1000); k < 1000000000; k *= 10 {
 
 		fmt.Println("\n### Max Cardinality", k)
-		fmt.Println("\n| Set1 | HLL1 | Set2 | HLL2 | S1 ∪ S2 | HLL1 ∪ HLL2 | S1 ∩ S2 | HLL1 ∩ HLL2 |")
-		fmt.Println("|---|---|---|---|---|---|---|---|")
+		fmt.Println("\n| Set1 | HMH1 | HLL1 | Set2 | HMH2 | HLL2 | S1 ∪ S2 | HMH1 ∪ HMH2 | HLL1 ∪ HLL2 | S1 ∩ S2 | HMH1 ∩ HMH2 | HLL1+HLL2-(HLL1∪HLL2) |")
+		fmt.Println("|---|---|---|---|---|---|---|---|---|---|---|---|")
 
 		for j := 1; j <= 5; j++ {
 
@@ -34,6 +35,9 @@ func main() {
 			size2 := rnd.Int63() % k
 			sk1 := hyperminhash.New()
 			sk2 := hyperminhash.New()
+
+			ll1 := hyperloglog.New()
+			ll2 := hyperloglog.New()
 
 			maxCol := size1
 			if maxCol > size2 {
@@ -46,7 +50,9 @@ func main() {
 
 			for i := 0; i < int(size1); i++ {
 				set[i]++
-				sk1.Add([]byte(strconv.Itoa(i)))
+				k := []byte(strconv.Itoa(i))
+				sk1.Add(k)
+				ll1.Insert(k)
 			}
 
 			for i := int(size1 - cols); i < int(size1-cols+size2); i++ {
@@ -54,7 +60,9 @@ func main() {
 				if set[i] > 1 {
 					intersections++
 				}
-				sk2.Add([]byte(strconv.Itoa(i)))
+				k := []byte(strconv.Itoa(i))
+				sk2.Add(k)
+				ll2.Insert(k)
 			}
 
 			card1 := sk1.Cardinality()
@@ -62,7 +70,31 @@ func main() {
 			ints1 := sk1.Intersection(sk2)
 			m := sk1.Merge(sk2)
 			mcard := m.Cardinality()
-			row := fmt.Sprintf("| %d | %d | %d | %d | %d | %d | **%d** (%f%%) | **%d** (%f%%) |", size1, card1, size2, card2, len(set), mcard, cols, float64(float64(100*cols)/float64(len(set))), ints1, 100*float64(ints1)/float64(mcard))
+
+			var ll1card, ll2card, llmcard, llints uint64
+			ll1card = ll1.Estimate()
+			ll2card = ll2.Estimate()
+			err := ll1.Merge(ll2)
+			if err == nil {
+				llmcard = ll1.Estimate()
+			}
+			if llmcard > 0 {
+				if llmcard < ll1card+ll2card {
+					llints = ll1card + ll2card - llmcard
+				}
+			} else {
+				llmcard, llints = 1, 0
+			}
+
+			row := fmt.Sprintf(
+				"| %d | %d | %d | %d | %d | %d | %d | %d | %d | **%d** (%f%%) | **%d** (%f%%) | **%d** (%f%%) |",
+				size1, card1, ll1card,
+				size2, card2, ll2card,
+				len(set), mcard, llmcard,
+				cols, float64(float64(100*cols)/float64(len(set))),
+				ints1, 100*float64(ints1)/float64(mcard),
+				llints, float64(llints)/float64(llmcard)*100,
+			)
 
 			fmt.Println(row)
 		}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/axiomhq/hyperminhash
 go 1.15
 
 require (
+	github.com/axiomhq/hyperloglog v0.0.0-20191112132149-a4c4c47bc57f
 	github.com/dgryski/go-metro v0.0.0-20200812162917-85c65e2d0165
 	github.com/dgryski/go-pcgr v0.0.0-20190219145045-9a7ede0ca611
 )
